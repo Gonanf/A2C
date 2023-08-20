cd311b47788c50884b66759192307f6fe7b04a4a
Gonanf <57074703+Gonanf@users.noreply.github.com>
2023-08-20 17:48:28 -0300

    A2C.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,6 @@
 static void Main(string[] args){
  //Por ahora imprimir cuantos argumentos hay, luego se puede hacer un switch para ver cual argumento dio
  Console.WriteLine(args.Length);
- 
+ //sexo
 }
 }
